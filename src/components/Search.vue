e48600b2fe5a685ecd46597f5dd95f8f392a0377
Code <Code>
2018-11-06 23:36:29 -0500

    Add api request to Search component from $route params

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,44 @@
 
 <template>
-  <div>
+  <div :key="$route.params.search_query" >
     <div 
-      v-loading="$root.loading"
+      v-loading="loading"
       element-loading-background="rgba(0, 0, 0, 0.8)"
       class="container">
-      <h1> Search results for {{ $root.search_query }} </h1>
-      <anime-list :animes="$root.search_results" />
+      <h1> Search results for {{ $route.params.search_query }} </h1>
+      <anime-list :animes="results" />
     </div>
   </div>
 </template>
 
 <script>
 import AnimeList from '@/components/modules/AnimeList'
+import maw from "@/myanimewatch";
 export default {
   name: 'Home',
   components: {
     AnimeList
+  },
+  data () {
+    return {
+      results: undefined,
+      loading: true,
+    }
+  },
+  methods: {
+    async search () {
+      this.loading = true;
+      this.results = undefined;
+      const response = await maw.searchAnime(this.$route.params.search_query);
+      this.results = response.results;
+      this.loading = false;
+    }
+  },
+  watch: {
+    '$route.params.search_query': {
+      handler: 'search',
+      immediate: true
+    }
   },
 }
 </script>
