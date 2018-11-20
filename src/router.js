5385c9c2ae24dc001013d2cf45977c148dc70939
Code <Code>
2018-11-19 19:10:26 -0500

    Update 404, index, base route to not be dynamic import

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/components/Home'
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { name: 'home',    component: () => import('@/components/Home'),    path: '/' },
+    { name: 'home',    component: Home,    path: '/' },
     { name: 'content', component: () => import('@/components/content/Content'), path: '/anime/:content_id',
       children: [
         { name: 'episode', component: () => import('@/components/content/Episode'), path: 'episode/:episode' },
